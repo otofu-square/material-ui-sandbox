e82d4077f7a589db43e52283d6792f47008a2f4a
otofu-square <otofu.square@gmail.com>
2019-05-22 00:51:01 +0900

    :tada: implement hello world button with material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
-import * as React from "react";
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
 
-const App = () => <div>Hello World from React</div>;
+const App = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);
 
-render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
